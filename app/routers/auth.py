3651fdc8ba6283dadecae6f1c97105f25ba90a71
Hernan Provoste <hernanprovostev@outlook.com>
2025-07-15 09:50:19 -0400

    Feat: Implement login endpoint and JWT generation

--- app/routers/auth.py
+++ app/routers/auth.py
@@ -1,7 +1,8 @@
 from fastapi import APIRouter, Depends, HTTPException, status
+from fastapi.security import OAuth2PasswordRequestForm
 from sqlalchemy.orm import Session
 
-from .. import database, schemas
+from .. import database, schemas, security
 from ..repositories.user_repository import UserRepository
 
 router = APIRouter(
@@ -22,3 +23,16 @@ def register(user: schemas.UserCreate, db: Session = Depends(get_db)):
     if db_user:
         raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="Email already registered")
     return user_repository.create_user(user)
+
+@router.post("/login/", response_model=schemas.Token)
+def login_for_access_token(form_data: OAuth2PasswordRequestForm = Depends(), db: Session = Depends(get_db)):
+    user_repository = UserRepository(db)
+    user = user_repository.get_user_by_email(email=form_data.username) # OAuth2 use username as email
+
+    # Verify that the user exists or the password is correct
+    if not user or not security.verify_password(form_data.password, user.hashed_password):
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Invalid Credentials", headers={"WWW-Authenticate": "Bearer"})
+
+    # Create token
+    access_token = security.create_access_token(data={"sub": user.email})
+    return {"access_token": access_token, "token_type": "bearer"}
--- app/security.py
+++ app/security.py
@@ -1,4 +1,7 @@
+from datetime import datetime, timedelta, timezone
+from jose import JWTError, jwt
 from passlib.context import CryptContext
+from .config import settings
 
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
@@ -7,3 +10,10 @@ def verify_password(plain_password: str, hashed_password: str) -> bool:
 
 def get_password_hash(password: str) -> str:
     return pwd_context.hash(password)
+
+def create_access_token(data: dict) -> str:
+    to_encode = data.copy()
+    expire = datetime.now(timezone.utc) + timedelta(minutes=settings.access_token_expire_minutes)
+    to_encode.update({"exp": expire})
+    encoded_jwt = jwt.encode(to_encode, settings.secret_key, algorithm=settings.algorithm)
+    return encoded_jwt
